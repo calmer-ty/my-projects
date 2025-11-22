5c9ea804da95647831589111b4a08b9117675703
Calmer <ty.calmer96@gmail.com>
2025-11-23 01:46:04 +0900

    feat: Home 베이직 완성

--- src/components/units/dashboard/index.tsx
+++ src/components/units/dashboard/index.tsx
@@ -1,86 +1,99 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-import DashboardDialog from "./DashboardDialog";
 import { Card } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
-import { projects } from "./data";
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight } from "lucide-react";
+import { Github } from "lucide-react";
+
+import DashboardDialog from "./DashboardDialog";
+
+import { projects } from "./data";
 
 export default function DashboardUI() {
   const [selectedId, setSelectedId] = useState<string | null>(null);
 
-  // const hasAnimatedRef = useRef(false);
-  // const MotionCarousel = motion(Carousel);
   const MotionCard = motion(Card);
   const MotionButton = motion(Button);
 
   return (
-    <article className="flex flex-col items-center">
-      {/* Hero */}
-      <section className="flex w-full h-screen snap-start">
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: { opacity: 0, y: 20 },
-            visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.4 } },
+    <article className="">
+      <motion.header
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        className="fixed flex justify-between items-center w-full h-16 px-8 bg-white z-10"
+      >
+        <h1 className="text-lg font-extrabold">TaeYeon.</h1>
+        <div className="flex gap-4">
+          <Button variant="link" className="hover:text-purple-600 transition-colors duration-300" onClick={() => document.getElementById("home")?.scrollIntoView({ behavior: "smooth" })}>
+            Home
+          </Button>
+          <Button variant="link" className="hover:text-purple-600 transition-colors duration-300" onClick={() => document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" })}>
+            Projects
+          </Button>
+          <Button variant="link" className="hover:text-purple-600 transition-colors duration-300" onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })}>
+            About
+          </Button>
+        </div>
+        <MotionButton
+          variant="outline"
+          className="w-10 h-10 rounded-full"
+          animate={{
+            // opacity: [1, 0.5, 1], // 깜박임
+            scale: [1, 1.2, 1], // 살짝 커졌다 작아짐
           }}
-          className="w-4/6 pt-20 pr-30 p-14"
-        >
-          <motion.p className="text-xl sm:text-2xl" variants={{ hidden: { y: 20, opacity: 0 }, visible: { y: 0, opacity: 1 } }}>
-            안녕하세요!
-          </motion.p>
-          <motion.p className="my-6 text-3xl sm:text-4xl font-extrabold leading-12" variants={{ hidden: { y: 20, opacity: 0 }, visible: { y: 0, opacity: 1 } }}>
-            프론트엔드 개발자 <span className="text-green-600">김태연</span>입니다.
-          </motion.p>
-          <motion.p className="my-6 text-lg sm:text-xl break-keep" variants={{ hidden: { y: 20, opacity: 0 }, visible: { y: 0, opacity: 1 } }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam deserunt iste, quasi quos culpa maxime totam soluta quo expedita, modi aliquid eius ratione, ullam quisquam pariatur?
-            Consequuntur nulla ea et.
-          </motion.p>
-          <MotionButton
-            className="mt-10"
-            variants={{ hidden: { y: 20, opacity: 0 }, visible: { y: 0, opacity: 1 } }}
-            onClick={() => document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" })}
-          >
-            <ArrowUpRight className="w-5 h-5 transition-transform group-hover:translate-x-1" />
-            <span>소개 보기</span>
-          </MotionButton>
-        </motion.div>
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: { opacity: 0, y: 20 },
-            visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.3 } },
+          transition={{
+            duration: 1.5,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "easeInOut",
           }}
-          className="w-2/6 pt-20 p-14 bg-sky-100"
         >
-          <div className="relative w-86 h-130 mt-10">
-            {/* 뒤 레이어 */}
-            <div className="absolute top-0 left-0 w-full h-full bg-gray-300 rounded-full rotate-45 z-0" />
+          <Github size={10} />
+        </MotionButton>
+      </motion.header>
 
-            {/* 앞 카드 */}
-            <img
-              src="/images/sample.jpg"
-              alt="sample"
-              className="relative w-full h-full object-cover rounded-full z-10 shadow-lg 
-                transition-transform duration-300 hover:scale-105 hover:translate-x-2 hover:translate-y-2"
-            />
+      {/* Hero */}
+      <section id="home" className="flex h-screen pt-16 snap-start">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 1.0, ease: "easeOut" }}
+          className="flex justify-between w-full pt-40 pr-30 p-20"
+        >
+          {/* 글 내용 */}
+          <div className="max-w-170">
+            <p className="text-xl sm:text-2xl">안녕하세요!</p>
+            <p className="my-6 text-4xl sm:text-5xl font-extrabold leading-12">
+              프론트엔드 개발자 <span className="text-green-600">김태연</span>입니다.
+            </p>
+            <p className="my-6 text-lg sm:text-xl break-keep">
+              React를 중심으로 웹 서비스를 개발합니다.
+              <br />
+              사용자가 직접 쓰고 싶어 하는 경험과 가치를 고민합니다.
+            </p>
+            <div className="flex gap-4 mt-10">
+              <Button onClick={() => document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" })}>
+                <ArrowUpRight className="w-5 h-5 transition-transform group-hover:translate-x-1" />
+                <span>프로젝트 보기</span>
+              </Button>
+              <Button variant="secondary" onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })}>
+                <ArrowUpRight className="w-5 h-5 transition-transform group-hover:translate-x-1" />
+                <span>소개 보기</span>
+              </Button>
+            </div>
+          </div>
+          {/* 사진 */}
+          <div className="w-100 h-100 p-10 bg-white border border-gray-300 rounded-full shadow-lg">
+            <img src="/images/sample.jpg" alt="sample" className="w-full h-full object-cover rounded-full z-10 shadow-lg" />
           </div>
         </motion.div>
       </section>
 
-      {/* About (짧게) → 이 위치에 추가 */}
-      {/* <section className="w-full h-screen">
-        <p>안녕하세요, 사용자가 체감할 수 있는 편리함을 만드는 프론트엔드 개발자 김태연입니다.</p>
-        <p>React · Next.js · Firebase 기반으로 실제 서비스 기능 개발과 배포를 주도해 왔습니다.</p>
-        <p>기능 구현뿐 아니라 사용자 경험과 유지보수성을 고려한 개발을 중요하게 생각합니다.</p>
-      </section> */}
-
       {/* 프로젝트 */}
-      <section id="projects" className="w-full h-screen snap-start">
+      <section id="projects" className="w-full h-screen pt-16 snap-start">
         <motion.div initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, ease: "easeOut" }} className="w-full py-10 text-4xl">
           <h2 className="text-4xl font-bold">TAEYEON&apos;S PORTFOLIO</h2>
           <p className="mt-2 text-gray-500 text-lg">아이디어를 실현하고 협업한 다양한 프로젝트를 만나보세요.</p>
@@ -139,6 +152,13 @@ export default function DashboardUI() {
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
+      </section>
+
+      {/* About (짧게) → 이 위치에 추가 */}
+      <section id="about" className="w-full h-screen pt-16 snap-start">
+        <p>안녕하세요, 사용자가 체감할 수 있는 편리함을 만드는 프론트엔드 개발자 김태연입니다.</p>
+        <p>React · Next.js · Firebase 기반으로 실제 서비스 기능 개발과 배포를 주도해 왔습니다.</p>
+        <p>기능 구현뿐 아니라 사용자 경험과 유지보수성을 고려한 개발을 중요하게 생각합니다.</p>
       </section>
       {/* 모달 */}
       <DashboardDialog selectedId={selectedId} setSelectedId={setSelectedId} />
