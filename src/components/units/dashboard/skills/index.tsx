f666ddeebc3e81d1298f8d15876928d6542a525d
Calmer <ty.calmer96@gmail.com>
2025-12-10 13:37:59 +0900

    styles: 스킬 페이지 스타일 수정

--- src/components/units/dashboard/skills/index.tsx
+++ src/components/units/dashboard/skills/index.tsx
@@ -32,8 +32,9 @@ export default function DashboardSkills() {
                 key={item.label}
                 variant="outline"
                 onClick={() => setSelectedTab(item)}
-                className={`relative px-4 py-2 rounded-full ${item === selectedTab ? "bg-blue-500 text-white hover:bg-blue-400 hover:text-white" : ""} font-medium text-center
-                dark:${item === selectedTab ? "bg-blue-900/50" : "bg-blue-900/20"} dark:hover:bg-blue-900/60`}
+                className={`relative px-4 py-2 rounded-full ${
+                  item === selectedTab ? "bg-blue-400 dark:bg-blue-900/50 hover:bg-blue-300 dark:hover:bg-blue-900/70 text-white hover:text-white" : ""
+                } font-medium text-center`}
               >
                 <i className="inline-block align-middle mr-0 md:mr-2">{item.icon}</i>
                 <span className="hidden md:inline-block">{item.label}</span>
@@ -53,7 +54,7 @@ export default function DashboardSkills() {
               >
                 {selectedTab.points.map((point, idx) => (
                   <div key={idx} className="flex items-start flex-col md:flex-row gap-4 p-4 rounded-md break-keep">
-                    <span className="shrink-0 w-18 py-1 bg-blue-400 rounded-lg text-white text-center text-sm">Point {idx + 1}</span>
+                    <span className="shrink-0 w-18 py-1 bg-blue-400 dark:bg-blue-700/30 rounded-lg text-white text-center text-sm">Point {idx + 1}</span>
                     <div>
                       <h3 className="mb-1 font-bold">{point.title}</h3>
                       <p>{point.desc}</p>
